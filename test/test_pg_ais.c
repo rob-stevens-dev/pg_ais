81f8f2c54c65ce808a0c58ec82481b6e0a194bc7
Rob Stevens <robert.stevens.dev@gmail.com>
2025-05-31 21:45:23 -0400

    Updated Unit test to exercise expanded support for AIS Message Type 26.

--- test/test_pg_ais.c
+++ test/test_pg_ais.c
@@ -362,9 +362,14 @@ static void test_msg_26_parsing(void **state) {
     assert_true(ok);
     assert_int_equal(msg.type, 26);
     assert_int_equal(msg.mmsi, 123456789);  // Replace with expected MMSI
+    if (msg.structured) {
+        assert_true(msg.app_id != 0);
+    }
     assert_true(msg.bin_len > 0);
+    assert_non_null(msg.bin_data);
     free_ais_message(&msg);
 }
+
 
 static void test_msg_27_parsing(void **state) {
     AISMessage msg;
